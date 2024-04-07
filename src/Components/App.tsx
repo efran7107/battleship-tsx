e2d6e1f2d24aa71501ea9e024695646b60504106
efran7107 <efran7107@gmail.com>
2024-04-07 10:58:14 -0600

    wanted to test something

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,26 @@
-import './styles/App.css'
+import '../styles/App.css'
 
 function App() {
 
   return (
     <>
-     
+     <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        console.log(e.currentTarget.id);
+        
+      }}
+     action="" id="form-one">
+      <input type="submit" value="submit" />
+     </form>
+     <form 
+     onSubmit={(e) => {
+      e.preventDefault()
+      console.log(e.currentTarget.id);
+    }}
+     action="" id="form-two">
+     <input type="submit" value="submit" />
+     </form>
     </>
   )
 }
